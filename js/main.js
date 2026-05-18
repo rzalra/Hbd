0fc305a9094ad26ae28b829a4566cfa5308193dc
RIZAL NUGRAHANTO <rizalnxa929@gmail.com>
2026-05-19 05:01:43 +0700

    update

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Selamat Istirahat Sayanggg').split('')
+    const titles = ('Happy Birthday Sayangg').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
